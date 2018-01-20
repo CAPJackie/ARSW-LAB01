4b18296c2771131945303896127436294c0ae89f
CAPJackie <juan.ramirez-me@mail.escuelaing.edu.co>
2018-01-20 10:51:16 -0500

    Parte III Runtime Class

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -16,7 +16,8 @@ public class Main {
     
     public static void main(String a[]) throws InterruptedException{
         HostBlackListsValidator hblv=new HostBlackListsValidator();
-        List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55", 5);
+        List<Integer> blackListOcurrences=hblv.checkHost("212.24.24.55", 100);
+        System.out.println("CPU cores: "+ Runtime.getRuntime().availableProcessors());
         System.out.println("The host was found in the following blacklists:"+blackListOcurrences);
         
     }
